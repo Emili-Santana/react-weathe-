0e048187d204916ca29b6c2e6bfc5320ab8e3530
Seu Nome <seu.email@exemplo.com>
2024-12-03 17:13:38 +0000

    Updaded the links of Github and Netlify

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <Weather />
 
 
-<p>This project was coded by  <a href="https://github.com/Emili-Santana" target="_blank" rel="noreferrer">Emili Santana</a> and is open-sourced on <a href="https://github.com/Emili-Santana/Weather-App-Week-05" target="_blank" rel="noreferrer">Github</a> and hosted on <a href="https://best-weather-react.netlify.app/" target="_blank" rel="noreferrer"> Netlify </a>
+<p>This project was coded by  <a href="https://github.com/Emili-Santana" target="_blank" rel="noreferrer">Emili Santana</a> and is open-sourced on <a href="https://github.com/Emili-Santana/react-weathe-" target="_blank" rel="noreferrer">Github</a> and hosted on <a href="https://react-best-weather-app.netlify.app/" target="_blank" rel="noreferrer"> Netlify </a>
 </p>
     </div>
     </div>
