2269ca7236efb3543d54c3bffcfd5774c3420fdc
Seu Nome <seu.email@exemplo.com>
2024-12-03 17:04:24 +0000

    Added inicial struture of project and install Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Weather from './Weather.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather />
+
+
+<p>This project was coded by  <a href="https://github.com/Emili-Santana" target="_blank" rel="noreferrer">Emili Santana</a> and is open-sourced on <a href="https://github.com/Emili-Santana/Weather-App-Week-05" target="_blank" rel="noreferrer">Github</a> and hosted on <a href="https://best-weather-react.netlify.app/" target="_blank" rel="noreferrer"> Netlify </a>
+</p>
     </div>
+    </div>
+    
   );
 }
 
